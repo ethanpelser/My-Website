2f633665a85cd5e3a5d00698d577a7b9ea790d02
ethanpelser <ethan.pelser.me@gmail.com>
2024-12-11 17:02:19 +0200

    Update contact_me.js
    fizxed cors (hopfully)

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://eqegv6ygj5.execute-api.eu-west-1.amazonaws.com",
+                url: "https://tonqir50o0.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
