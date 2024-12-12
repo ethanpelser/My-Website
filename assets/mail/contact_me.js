29a1531ecef6f6f8a98d175b3db2d91b89b1e93a
ethanpelser <ethan.pelser.me@gmail.com>
2024-12-12 09:47:40 +0200

    Update contact_me.js
    idk

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://rcbixqzmwi.execute-api.eu-west-1.amazonaws.com",
+                url: "https://v2pewlxxs2.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
