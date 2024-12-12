69e717868079836746a4a628e3fc0fc6c04ed61d
ethanpelser <ethan.pelser.me@gmail.com>
2024-12-12 16:21:29 +0200

    Update contact_me.js
    cors everywhere proxy

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://zyfzli47jl.execute-api.eu-west-1.amazonaws.com/prune",
+                url: "https://cors-everywhere.zyfzli47jl.execute-api.eu-west-1.amazonaws.com/prune",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
