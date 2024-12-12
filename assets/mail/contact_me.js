51a457f5159ab1b36499be46b7a704e8c1135384
ethanpelser <ethan.pelser.me@gmail.com>
2024-12-12 09:27:22 +0200

    Update contact_me.js
    cant get cors to work lol

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://kye98h1im0.execute-api.eu-west-1.amazonaws.com/default/cloud-compute-predict-lambda",
+                url: "https://rcbixqzmwi.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
