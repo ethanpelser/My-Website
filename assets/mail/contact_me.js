a895b54604e5e3c6375d1516a18d223ec2f8a061
ethanpelser <ethan.pelser.me@gmail.com>
2024-12-12 09:13:26 +0200

    new api
    new api

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://tonqir50o0.execute-api.eu-west-1.amazonaws.com",
+                url: "https://kye98h1im0.execute-api.eu-west-1.amazonaws.com/default/cloud-compute-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
